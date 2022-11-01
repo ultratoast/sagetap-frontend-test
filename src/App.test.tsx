d9ed212fb50cee4f0f75368a2988e3f8546293f0
Alana <alanatotzke@gmail.com>
2022-11-01 14:21:53 -0400

    Feat: frontend development test code for Ally Totzke

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,13 @@
-import { render, screen , fireEvent } from '@testing-library/react';
+import { render, screen ,  waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { App, ArtItem } from './App';
+
+function setup(jsx: any) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  }
+}
 
 test('has title', () => {
   render(<App />);
@@ -7,16 +15,57 @@ test('has title', () => {
   expect(title).toBeInTheDocument();
 });
 
-test('for an art item, submit button is disabled until a rating is selected', () => {
+test('for an art item, submit button is disabled until a rating is selected', async () => {
+  const { user } = setup(<App />)
+
+  const submitButtons = await screen.findAllByText('Submit');
+  const submitButton = submitButtons[0]
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton).toBeDisabled();
+
+  const rateButtons = await screen.findAllByText('1');
+  await user.click(rateButtons[0]);
+  await waitFor(() => expect(submitButton).not.toBeDisabled());
 });
 
-test('for an art item, clicking numbered button updates rating display below image to be that number', () => {
+test('for an art item, clicking numbered button updates rating display below image to be that number', async () => {
+  const { user } = setup(<App />)
+
+  const rateButtons1 = await screen.findAllByText('1');
+  await user.click(rateButtons1[0]);
+  const rateText = await screen.findByText('Rating: 1');
+  await expect(rateText).toBeInTheDocument();
 });
 
-test('for an art item, clicking numbered button updates rating display below image to be that number, clicking two different numbers one after the other', () => {
+test('for an art item, clicking numbered button updates rating display below image to be that number, clicking two different numbers one after the other', async () => {
+  const { user } = setup(<App />)
+
+  const rateButtons1 = await screen.findAllByText('1');
+  await user.click(rateButtons1[0]);
+  const rateText = await screen.findByText('Rating: 1');
+  await expect(rateText).toBeInTheDocument();
+  const rateButtons2 = await screen.findAllByText('2');
+  await user.click(rateButtons2[0]);
+  const rateText2 = await screen.findByText('Rating: 2');
+  await expect(rateText2).toBeInTheDocument();
+
 });
 
-test('for an art item, clicking submit POSTs update, displays a toast success message, hides buttons', () => {
+test('for an art item, clicking submit POSTs update, displays a toast success message, hides buttons', async () => {
   // The endpoint and payload for the submit button can be found in the submit method in `App.tsx`.
   // For the purpose of this test, please use a mock function instead.
+
+//  const sendRating = () {
+//    return {message: "Success"};
+//  }
+
+//  const { user } = setup(<App />)
+
+//  const submitButtons = await screen.findAllByText('Submit');
+//  const submitButton = submitButtons[0]
+
+//  const rateButtons = await screen.findAllByText('1');
+//  await user.click(rateButtons[0]);
+//  await user.click(submitButton);
+//  await expect(sendRating.mock).toHaveBeenCalled()
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,192 @@
  /* eslint-disable */
 
 import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Input from '@mui/material/Input';
+import Popper from '@mui/material/Popper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Fade from '@mui/material/Fade';
+import Paper from '@mui/material/Paper';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
+const SUBMISSION_FAILURE = 'There has been an error, please try again';
+
 async function getArtwork(id: number) {
-  return fetch('https://api.artic.edu/api/v1/artworks/' + id);
+  return fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+    .then((response: any) => response.json())
+    .then((response:any) => 
+      response.data
+    )
+    .catch((error:any) =>
+      []
+    )
 }
 
-function getImageUrl(id: string) {
-  return 'https://www.artic.edu/iiif/2/' + id + '/full/843,/0/default.jpg'
+async function getImageUrl(id: string) {
+  return fetch(`https://www.artic.edu/iiif/2/${id}/full/843,/0/default.jpg`)
+    .then((response:any) => response.blob())
+    .then((blob:any) =>
+      URL.createObjectURL(blob)
+    )
+    .catch((error:any) => 
+      ''
+    );
 }
+
+async function submitRating(id: string, rating: number) {
+  return fetch(
+    'https://v0867.mocklab.io/rating',
+    { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        rating
+      })
+    })
+    .then((response: any) => response.json())
+    .then((response: any) => 
+      response.message
+    )
+    .catch((error: any) => 
+      SUBMISSION_FAILURE
+    );
+}
+
 
 function ArtItem(props: any) {
-  const [voted, setVoted] = useState<boolean>(false)
-  const [artwork, setArtwork] = useState<any>(null)
-
-  const submit = () => {
-    console.log("Submitting!")
-    /* 
-    Please have the submit button POST to https://v0867.mocklab.io/rating with the following payload:
-
-      {
-        "id": {#id},
-        "rating": {#rating}
-      }
-
-    Where id is the artwork's id, and rating is the selected rating.
-
-    The endpoint should return the following:
-
-    {
-      "message": "Success"
-    }
-  */
-    return () => {};
-  };
+  const [voted, setVoted] = useState<boolean>(false);
+  const [artwork, setArtwork] = useState<any>(null);
+  const [rating, setRating] = useState<any>(null);
+  const [artworkImage, setArtworkImage] = useState<any>(null);
+  const [message, setMessage] = useState<any>(null);
 
   if (props.disabled)
   {
     return <></>;
   }
   
-  useEffect( () => {
-    getArtwork(props.id).then(r => r.json()).then(json => setArtwork(json))
-  }, []);
+  const handleSubmit = (id: string, rating: number) => {
+    submitRating(id, rating)
+    .then((response: any) =>
+        setMessage(response)
+      );
+  };
 
+  const handleVote = (rg: number) => {
+    setRating(rg)
+    setVoted(true);
+  };
+
+  const handleCloseToast = () => {
+    setMessage(null);
+  }
+ 
+  useEffect(() => {
+    if (!artwork || artwork.id !== props.id) {
+      getArtwork(props.id)
+        .then((response: any) =>
+          setArtwork(response)
+        );
+    }
+  }, [artwork]);
+ 
+  useEffect(() => {
+    if ((artwork && !artworkImage) || (artwork && artwork.id !== props.id)) {
+      getImageUrl(artwork.image_id)
+        .then((response: any) => 
+          setArtworkImage(response)
+        );
+    }
+  }, [artworkImage]);
+  
+  const ratings = [1, 2, 3, 4, 5];
+  
   return (
-    <div className="item">
-        <h2>{artwork && artwork.data.title}</h2>
-        <h3>{artwork && artwork.data.artist_title}</h3>
-        <img style={ { width: 100 } } src={artwork != null ? getImageUrl(artwork.data.image_id) : ""} />
-        <p>Rating: {artwork && artwork.rating}</p>
-        <button onClick={() => { artwork.rating = 1; setVoted(true); }}>1</button>
-        <button onClick={() => { artwork.rating = 2; setVoted(true); }}>2</button>
-        <button onClick={() => { artwork.rating = 3; setVoted(true); }}>3</button>
-        <button onClick={() => { artwork.rating = 4; setVoted(true); }}>4</button>
-        <button onClick={() => { artwork.rating = 5; setVoted(true); }}>5</button>
-        <button onClick={submit()}>Submit</button>
+    <div className="artwork-item" id={`artwork-${props.id}`}>
+      <h2>{artwork && artwork.title ? artwork.title : 'untitled'}</h2>
+      <h3>{artwork && artwork.artist_title ? artwork.artist_title : 'anonymous'}</h3>
+      {
+        artwork && artworkImage ? 
+        <img style={ { width: '66%' } } src={artworkImage} /> : 
+        <strong className="loading-spinner">Image loading</strong>
+      }
+      <p>Rating: {rating ? rating : 'not yet rated'}</p>
+      {ratings.map((rg:number) =>
+        <Button onClick={() => { handleVote(rg) }}>{rg}</Button>
+      )}
+      <Button disabled={!voted} onClick={() => {handleSubmit(artwork.id, rating)}}>Submit</Button>
+      {voted && message && <Popper sx={{left: 'calc(50% - 100px) !important', top: 'calc(50% - 30px) !important'}} open={true} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Button onClick={handleCloseToast} className="close-toast">X</Button>
+              <Typography sx={{ p: 2 }}>{message}</Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>}
     </div>
   )
 }
 
 function App() {
   const [arts, setArts] = useState<any>([])
-  
-  const a = [
-    { id: 27992, disabled: false },
-    { id: 27998, disabled: false },
-    { id: 27999, disabled: false },
-    { id: 27997, disabled: true },
-    { id: 27993, disabled: false },
+  const [artInput, setArtInput] = useState<any>("");
+
+  const handleRemoveArtwork = (id: string) => {
+    const filteredarts = arts.filter((art: any) => art.id !== id);
+    setArts(filteredarts);
+  };
+
+  const handleInputChange = (event: any) => {
+    setArtInput(event.target.value);
+  };
+
+  const handleAddArtwork = (event: any) => {
+    event.preventDefault();
+    const newart = { id: artInput, disabled: false }
+    setArts((prevState: any) => [...prevState,newart]);
+  }
+   
+  const newarts = [
+    { id: '27992', disabled: false },
+    { id: '27998', disabled: false },
+    { id: '27999', disabled: false },
+    { id: '27997', disabled: false },
+    { id: '27993', disabled: false },
   ];
-
   useEffect(() => {
-    const temp = []
-    for (let i = 0; i < a.length; i++) {
-      temp.push(<ArtItem id={a[i].id} disabled={a[i].disabled}></ArtItem>)
+    if (!arts.length) {
+      setArts(newarts);
     }
-    setArts(temp)
-  }, [setArts])
-
+  });
   return (
-    <div className="App">
+    <Container maxWidth="lg">
       <h1>Art Rater</h1>
-      {arts}
-    </div>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+        {arts && arts.map((item: any) =>
+          <Grid item xs={12}>
+            <ArtItem id={item.id} disabled={item.disabled} />
+            <Button color="error" onClick={() => {handleRemoveArtwork(item.id)}}>Remove Art</Button>
+          </Grid>
+        )}
+        <div>
+          <Input placeholder="Artwork id" type="text" name="id" onChange={handleInputChange}/>
+          <Button onClick={handleAddArtwork}>Add art</Button>
+        </div>
+      </Grid>
+    </Container>
   );
 }
 
